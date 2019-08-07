a7722a097f1b71726ea2112104606925161fdaca
Andy <>
2019-08-07 11:38:17 +0800

    註冊 登入 頁面切換

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-
-  constructor() { }
+  authType: string = '';
+  title: string = '';
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+      this.route.url.subscribe(data => {
+      // Get the last piece of the URL (it's either 'login' or 'register')
+      this.authType = data[data.length - 1].path;
+      // Set a title for the page accordingly
+      this.title = (this.authType === 'login') ? 'Sign in' : 'Sign up';
+  });
   }
-
 }
