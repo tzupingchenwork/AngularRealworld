78c4a5d671e7ee6051dc8a6a1a0f1dc9390cac4b
Andy <>
2019-08-08 09:50:05 +0800

    取得user資料 建立id

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -9,12 +9,12 @@ export class InMemoryDataService {
   constructor() { }
   createDb() {
     const users = [
-      { email: 'aa@com', username: 'Andy', bio: 'hello' },
-      { email: 'bb@com', username: 'Abby', bio: 'hello' },
+      { id: '1', email: 'aa@com', username: 'Andy', bio: 'hello' },
+      { id: '2', email: 'bb@com', username: 'Abby', bio: 'hello' },
     ];
     const article = [
-      {slug : 'slug1', title: 'title1', description: 'description1' , body: 'body1' },
-      {slug : 'slug2', title: 'title2', description: 'description2' , body: 'body2' }
+      {id: '1', slug : 'slug1', title: 'title1', description: 'description1' , body: 'body1' },
+      {id: '2', slug : 'slug2', title: 'title2', description: 'description2' , body: 'body2' }
     ];
     return {users, article};
   }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -3,11 +3,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { User } from '../shared/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() {}
+  constructor( private http: HttpClient) {
+  }
+  private usersUrl = 'api/users';  // URL to web api
 
+  // 取得user資料
+  getUser(id: number): Observable<User> {
+    const url = `${this.usersUrl}/${id}`;
+    return this.http.get<User>(url);
+  }
 }
