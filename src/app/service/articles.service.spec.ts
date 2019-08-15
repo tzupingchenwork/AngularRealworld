cfff968bcd52f70e84eaab311be9fae38a1949f9
Andy <>
2019-08-15 17:37:28 +0800

    新增 修改  刪除 的測試

--- src/app/service/articles.service.spec.ts
+++ src/app/service/articles.service.spec.ts
@@ -28,14 +28,6 @@ describe('service: ArticlesService', () => {
     fixture = TestBed.createComponent(ArticleComponent);
     component = fixture.componentInstance;
     service = TestBed.get(ArticlesService);
-    spyOn(service, 'getArticle').and.returnValue(
-      of({
-        id: 1,
-        title: 'title1',
-        description: 'description1',
-        body: 'body1'
-      })   // Observable
-    );
     fixture.detectChanges();
   });
 
@@ -44,12 +36,91 @@ describe('service: ArticlesService', () => {
   });
 
   it('取得文章列表', () => {
-    // const http = HttpClientTestingModule;
+    spyOn(service, 'getArticles').and.returnValue(
+      of({
+        id: 1,
+        title: 'title1',
+        description: 'description1',
+        body: 'body1'
+      })   // Observable
+    );
     service.getArticles().subscribe(res => {
       expect(res[0].id).toBe(1);
       expect(res[0].title).toBe('title1');
       expect(res[0].description).toBe('description1');
       expect(res[0].body).toBe('body1');
+    });
+  });
+
+  it('取得id:1文章', () => {
+    spyOn(service, 'getArticle').and.returnValue(
+      of({
+        id: 1,
+        title: 'title1',
+        description: 'description1',
+        body: 'body1'
+      })   // Observable
+    );
+
+    service.getArticle(1).subscribe(res => {
+      expect(res.title).toBe('title1');
+      expect(res.description).toBe('description1');
+      expect(res.body).toBe('body1');
+    });
+  });
+
+  it('新增文章', () => {
+    spyOn(service, 'addArticle').and.returnValue(
+      of({
+        id: 1,
+        title: 'title1',
+        description: 'description1',
+        body: 'body1'
+      })   // Observable
+    );
+    const article = {
+      id: 1,
+      title: 'title1',
+      description: 'description1',
+      body: 'body1'};
+    service.addArticle(article).subscribe(res => {
+      expect(res.title).toBe('title1');
+      expect(res.description).toBe('description1');
+      expect(res.body).toBe('body1');
+    });
+  });
+
+  it('刪除文章', () => {
+    spyOn(service, 'deleteArticle').and.returnValue(
+      of(null)
+    );
+    const id = 1;
+    service.deleteArticle(id).subscribe(res => {
+      expect(res.title).toBeFalsy();
+      expect(res.description).toBeFalsy();
+      expect(res.body).toBeFalsy();
+    });
+  });
+
+  it('修改id:1 文章', () => {
+    spyOn(service, 'updateArticle').and.returnValue(
+      of({
+        id: 1,
+        title: 'N-title',
+        description: 'N-description',
+        body: 'N-body'
+      })   // Observable
+    );
+    const article = {
+      id: 1,
+        title: 'N-title',
+        description: 'N-description',
+        body: 'N-body'
+      };
+    service.updateArticle(article).subscribe(res => {
+      expect(res.title).toBe('N-title');
+      expect(res.description).toBe('N-description');
+      expect(res.body).toBe('N-body');
     });
   });
 
